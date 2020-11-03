f42953f6ef13df8155c126530e0650bf9cf034ad
Aini <71223978+Aini25@users.noreply.github.com>
2020-11-03 14:35:43 +0700

    Update TebakAngka.java

--- TebakAngka/src/tebakangka/TebakAngka.java
+++ TebakAngka/src/tebakangka/TebakAngka.java
@@ -16,13 +16,18 @@ public class TebakAngka {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Game2 A = new Game2();
+        Game1 A = new Game1();
+        System.out.println("Ayo Main Tebak Angka!");
+        System.out.println("****************************");
+        A.tampil();
+        
+        Game2 AA = new Game2();
         System.out.println("Ayo Main Tebak Angka!");
         System.out.println("Score Awal Kamu = 100");
         System.out.println("Salah -2");
         System.out.println("Dan jika benar di bawah 5 tebakan akan mendapat bonus 50");
         System.out.println("-------------------------------");
-        A.tampil2();
+        AA.tampil2();
 
     }
     
